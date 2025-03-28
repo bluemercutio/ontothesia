d894361dea2145af583003a60abf2c3c6987273c
CheCoelho <checoelho@gmail.com>
2025-03-28 15:36:46 +0200

    Update project info

--- src/app/project/copyWrite.ts
+++ src/app/project/copyWrite.ts
@@ -7,8 +7,17 @@ export const projectInfo = [
 
 On the surface *Ontothesia* can be regarded as an experiment in *Infinite Cinema* / *Infinite Gallery.* More profoundly it is an exploration of Posthuman Consciousness, cultural cartography, and the inherently pluralistic nature of The Universe.`,
   },
+
   {
     title: "The Archive",
-    paragraph: `At the core of Ontothesia is the concept of the archive and the work of the librarian.  While the end result is a novel immersive engagement with a memetic hyper-object, the latent coordinates of this representation are generated through the curation of cultural artefacts. In this way its representations are neither exhaustive nor extremely particular. Rather, they depict the perturbations and hauntings, the patterned themes, and the semantic & memetic shapes present in the archive. `,
+    paragraph: `At the core of Ontothesia is the concept of the archive and the work of the librarian. While the end result is a novel immersive engagement with a memetic hyper-object, the latent coordinates of this representation are generated through the curation of cultural artefacts. In this way its representations are neither exhaustive nor extremely particular. Rather, they depict the perturbations and hauntings, the patterned themes, and the semantic & memetic shapes present in the archive. In this way, the archive is not just a praxis of remembering, but also a praxis of imagining, of creation.`,
+  },
+  {
+    title: "Cultural Cartography",
+    paragraph: `At its core, *Ontothesia* is an exercise in cultural cartography, of developing ontological maps of a given narrative space and representing the memetic field of that narrative space through a series of immersive visual experiences. The core thesis of the project is that purely geographical representations of land and space is an insufficient lens for understanding culture, epigenetics, and identity.  This thesis can be viscerally expressed in terms of the African continent, where land and ethnic groups have been displaced, extracted, exploited, and existentially identified by colonially imposed borders and external forces. The actual identity of the people of Africa can be understood far more clearly by understanding culture, and the movement of culture across space and time.  *Ontothesia* attempts to create cultural maps by processing large sets of text that transcend both borders and centuries to develop what is herein referred to as a *hyperobject*, an assemblage of qualities and memetic fragments that articulate a living and extremely complex cultural entity."`,
+  },
+  {
+    title: "Posthuman Alternative Intelligence",
+    paragraph: `*Ontothesia* draws from thinkers such as *Bayo Akomolafe* who advocates for a posthumanist lens of identity, which is both an ideal and a conceptual exploration. Posthumanism acknowledges that our identities are fundamentally relational. This notion echoes the African understanding of personhood known as *Ubuntu*, and proposes a a view of our existential contours that encapsulates the more than human. The extensive complexity of biologies and the epigenetic forces that engages the natural world around us are included in this understanding as a part of our identity. *Ontothesia* aims to generate experiences that instantiate forms from these pothuman entities through the entry point of cultural artefacts that span and describe life and reality across centuries on the African continent.  *Ontothesia* also offers a framing of AI, artificial Intelligence, as *Alternative Intelligence*. We can understand traditional artificial intelligence such as large language models as *interfaces* to other *kinds* of intelligence.`,
   },
 ];
